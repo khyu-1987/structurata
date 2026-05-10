3bb71fcf0d8da8b96a2f203f022829faeb49cb76
Yurii Khutornyi <khyu1987@gmail.com>
2026-05-10 20:55:51 +0300

    Client constants to module scope

--- sdk/client.py
+++ sdk/client.py
@@ -1,19 +1,20 @@
 import requests
 
+API_BASE_URL = 'https://api.hunter.io/v2/'
+API_EMAIL_FINDER_PATH = 'email-finder'
+API_EMAIL_VERIFIER_PATH = 'email-verifier'
+API_TIMEOUT = 10
+
 
 class HunterClient:
 
-    API_BASE_URL = 'https://api.hunter.io/v2/'
-    API_EMAIL_FINDER_PATH = 'email-finder'
-    API_EMAIL_VERIFIER_PATH = 'email-verifier'
-    API_TIMEOUT = 10
     EMAIL_VALID_STATUS = 'valid'
 
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
 
     def find_email(self, domain: str, first_name: str, last_name: str) -> dict:
-        url = f'{self.API_BASE_URL}{self.API_EMAIL_FINDER_PATH}'
+        url = f'{API_BASE_URL}{API_EMAIL_FINDER_PATH}'
         query_params = {
             'domain': domain,
             'first_name': first_name,
@@ -22,12 +23,12 @@ class HunterClient:
         return self._make_request(url, query_params)
 
     def verify_email(self, email: str) -> dict:
-        url = f'{self.API_BASE_URL}{self.API_EMAIL_VERIFIER_PATH}'
+        url = f'{API_BASE_URL}{API_EMAIL_VERIFIER_PATH}'
         query_params = {'email': email}
         return self._make_request(url, query_params)
 
     def _make_request(self, url: str, query_params: dict) -> dict:
         headers = {'Authorization': f'Bearer {self.api_key}'}
-        response = requests.get(url, headers=headers, params=query_params, timeout=self.API_TIMEOUT)
+        response = requests.get(url, headers=headers, params=query_params, timeout=API_TIMEOUT)
         response.raise_for_status()
         return response.json()
