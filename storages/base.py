fe4a1bc344b34ce45bfff1e56725f149f739d41b
Yurii Khutornyi <khyu1987@gmail.com>
2026-05-13 19:58:47 +0300

    Generic storage record type

--- storages/base.py
+++ storages/base.py
@@ -1,22 +1,25 @@
 from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+RecordType = TypeVar('RecordType')
 
 
-class BaseStorage(ABC):
+class BaseStorage(ABC, Generic[RecordType]):
 
     @abstractmethod
-    def create(self, key: str, record: dict) -> None:
+    def create(self, key: str, record: RecordType) -> None:
         ...
 
     @abstractmethod
-    def read(self, key: str) -> dict | None:
+    def read(self, key: str) -> RecordType | None:
         ...
 
     @abstractmethod
-    def read_all(self) -> list[dict]:
+    def read_all(self) -> list[RecordType]:
         ...
 
     @abstractmethod
-    def update(self, key: str, record: dict) -> bool:
+    def update(self, key: str, record: RecordType) -> bool:
         ...
 
     @abstractmethod
--- storages/in_memory.py
+++ storages/in_memory.py
@@ -1,21 +1,25 @@
+from typing import Generic, TypeVar
+
 from storages.base import BaseStorage
 
+RecordType = TypeVar('RecordType')
 
-class InMemoryStorage(BaseStorage):
+
+class InMemoryStorage(BaseStorage[RecordType], Generic[RecordType]):
 
     def __init__(self) -> None:
-        self._storage: dict = {}
+        self._storage: dict[str, RecordType] = {}
 
-    def create(self, key: str, record: dict) -> None:
+    def create(self, key: str, record: RecordType) -> None:
         self._storage[key] = record
 
-    def read(self, key: str) -> dict | None:
+    def read(self, key: str) -> RecordType | None:
         return self._storage.get(key)
 
-    def read_all(self) -> list[dict]:
+    def read_all(self) -> list[RecordType]:
         return list(self._storage.values())
 
-    def update(self, key: str, record: dict) -> bool:
+    def update(self, key: str, record: RecordType) -> bool:
         if key not in self._storage:
             return False
         self._storage[key] = record
